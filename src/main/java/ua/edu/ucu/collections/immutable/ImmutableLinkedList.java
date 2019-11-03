d328fcff86ae2177071ffacada1406490289d2a4
Max <hrytsay@ucu.edu.ua>
2019-11-03 12:43:48 +0200

    ImmutableLinkedList done

--- src/main/java/ua/edu/ucu/collections/immutable/ImmutableLinkedList.java
+++ src/main/java/ua/edu/ucu/collections/immutable/ImmutableLinkedList.java
@@ -4,34 +4,36 @@ import java.util.Iterator;
 
 public class ImmutableLinkedList implements ImmutableList{
     private Node root = new Node();
+    private int size;
 
-    public ImmutableLinkedList() {}
+    public ImmutableLinkedList() {size = 0;}
 
     public ImmutableLinkedList(Node root) {this.root = root;}
 
     private ImmutableLinkedList copy() {
-        Node current = root;
         Node currentCopy = root.copy();
         Node rootCopy = currentCopy;
-        while (current != null) {
-            currentCopy.setNext(current.getNext().copy());
-            currentCopy = current.getNext();
-            current = current.getNext();
+        while (currentCopy != null) {
+            currentCopy.setNext(currentCopy.getNext().copy());
+            currentCopy = currentCopy.getNext();
         }
         return new ImmutableLinkedList(rootCopy);
     }
 
     private HalfCopy halfCopy(int index) {
-        Node current = root;
         Node currentCopy = root.copy();
+        Node rootCopy = currentCopy;
         for (int i = index; i > 0; i--) {
-            currentCopy.setNext(current.getNext().copy());
+            currentCopy.setNext(currentCopy.getNext().copy());
             currentCopy = currentCopy.getNext();
             if (currentCopy == null) {
                 throw new IndexOutOfBoundsException();
             }
         }
-        return new HalfCopy(new ImmutableLinkedList(currentCopy), currentCopy.getNext());
+        Node uncopied = currentCopy.getNext();
+        currentCopy.setNext(null);
+        return new HalfCopy(new ImmutableLinkedList(rootCopy), uncopied,
+                currentCopy);
     }
 
     @Override
@@ -45,76 +47,118 @@ public class ImmutableLinkedList implements ImmutableList{
 
     @Override
     public ImmutableLinkedList add(int index, Object e) {
-        Node current = root;
-        Node currentCopy = root.copy();
-        Node rootCopy = currentCopy;
-        for (int i = index - 1; i > 0; i++) {
-            currentCopy.setNext(current.getNext().copy());
-            currentCopy = currentCopy.getNext();
-            if (currentCopy == null) {
-                throw new IndexOutOfBoundsException();
-            }
-        }
+        HalfCopy hf = halfCopy(index);
+        ImmutableLinkedList head = hf.getHalfCopy();
         ImmutableLinkedList tail = new ImmutableLinkedList(
-                currentCopy.getNext()).copy();
-        Node toAdd = new Node(e);
-        toAdd.setNext(tail.getRoot());
-        currentCopy.setNext(toAdd);
-        return new ImmutableLinkedList(rootCopy);
+                hf.getUnCopied()).copy();
+        hf.getLastCopied().setNext(tail.getRoot());
+        return head;
     }
 
     @Override
     public ImmutableList addAll(Object[] c) {
         ImmutableLinkedList res = this.copy();
         for (int i = 0; i < c.length; i++) {
-            res.add(c[i]);
+            Node newRoot = new Node(c[i]);
+            newRoot.setNext(res.getRoot());
+            res.setRoot(newRoot);
         }
         return res;
     }
 
     @Override
     public ImmutableList addAll(int index, Object[] c) {
-        return null;
+        HalfCopy hf = halfCopy(index);
+        ImmutableLinkedList head = hf.getHalfCopy();
+        ImmutableLinkedList tail = new ImmutableLinkedList(
+                hf.getUnCopied()).copy();
+        tail.addAll(c);
+        hf.getLastCopied().setNext(tail.getRoot());
+        return head;
     }
 
     @Override
     public Object get(int index) {
-        return null;
+        Node current = root;
+        for (int i = index; i > 0; i--) {
+            current = current.getNext();
+            if (current == null) {
+                throw new IndexOutOfBoundsException();
+            }
+        }
+        return current.getValue();
     }
 
     @Override
     public ImmutableList remove(int index) {
-        return null;
+        HalfCopy hf = halfCopy(index);
+        ImmutableLinkedList head = hf.getHalfCopy();
+        ImmutableLinkedList tail = new ImmutableLinkedList(
+                hf.getUnCopied().getNext()).copy();
+        hf.getLastCopied().setNext(tail.getRoot());
+        return head;
     }
 
     @Override
     public ImmutableList set(int index, Object e) {
-        return null;
+        HalfCopy hf = halfCopy(index);
+        ImmutableLinkedList head = hf.getHalfCopy();
+        Node firstUncopied = hf.getUnCopied();
+        firstUncopied.setValue(e);
+        ImmutableLinkedList tail = new ImmutableLinkedList(
+                firstUncopied).copy();
+        hf.getLastCopied().setNext(tail.getRoot());
+        return head;
     }
 
     @Override
     public int indexOf(Object e) {
-        return 0;
+        Node current = root;
+        int counter = 0;
+        while (current != null) {
+            if (current.getValue() == e) {
+                return counter;
+            }
+            current = current.getNext();
+            counter++;
+        }
+        return counter;
     }
 
     @Override
     public int size() {
-        return 0;
+        int counter = 0;
+        Node current = root;
+        while (current != null) {
+            current = current.getNext();
+            counter++;
+        }
+        return counter;
     }
 
     @Override
     public ImmutableList clear() {
-        return null;
+        return new ImmutableLinkedList();
     }
 
     @Override
     public boolean isEmpty() {
+        if (root == null) {
+            return true;
+        }
         return false;
     }
 
     @Override
     public Object[] toArray() {
-        return new Object[0];
+        int len = size();
+        Object[] res = new Object [len];
+        Node current = root;
+        for (int i = 0; i < len; i++) {
+            res[i] = current;
+            current = current.getNext();
+        }
+        return res;
     }
 
     public void setRoot(Node root) {
