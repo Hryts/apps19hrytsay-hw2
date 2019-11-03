3db99a67438dfc346baf46b42ad7ffaaf712e675
Max <hrytsay@ucu.edu.ua>
2019-11-03 19:42:21 +0200

    Stack added

--- src/main/java/ua/edu/ucu/collections/Stack.java
+++ src/main/java/ua/edu/ucu/collections/Stack.java
@@ -1,5 +1,23 @@
 package ua.edu.ucu.collections;
 
+import ua.edu.ucu.collections.immutable.ImmutableLinkedList;
+
 public class Stack {
-    
+    private ImmutableLinkedList stack;
+
+    public Stack(ImmutableLinkedList stack) {
+        this.stack = stack;
+    }
+
+    public Object peek() {
+        return stack.get(0);
+    }
+
+    public Object dequeue() {
+        return stack.remove(0);
+    }
+
+    public void enqueue(Object e) {
+        stack.add(0, e);
+    }
 }
