d7f42fe002138882899216976ab1c62a08b86609
Max <hrytsay@ucu.edu.ua>
2019-11-03 19:42:00 +0200

    Queue added

--- src/main/java/ua/edu/ucu/collections/Queue.java
+++ src/main/java/ua/edu/ucu/collections/Queue.java
@@ -1,5 +1,23 @@
 package ua.edu.ucu.collections;
 
+import ua.edu.ucu.collections.immutable.ImmutableLinkedList;
+
 public class Queue {
-    
+    private ImmutableLinkedList queue;
+
+    public Queue(ImmutableLinkedList queue) {
+        this.queue = queue;
+    }
+
+    public Object peek() {
+        return queue.get(0);
+    }
+
+    public Object dequeue() {
+        return queue.remove(0);
+    }
+
+    public void enqueue (Object e) {
+        queue.add(queue.size()-1, e);
+    }
 }
