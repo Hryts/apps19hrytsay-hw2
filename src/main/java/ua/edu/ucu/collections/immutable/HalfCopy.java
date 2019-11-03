5401f9011ac6bbe0785ed3eb0e263696a98b195e
Max <hrytsay@ucu.edu.ua>
2019-11-03 12:43:19 +0200

    HalfCopy class added

--- src/main/java/ua/edu/ucu/collections/immutable/HalfCopy.java
+++ src/main/java/ua/edu/ucu/collections/immutable/HalfCopy.java
@@ -1,11 +1,51 @@
 package ua.edu.ucu.collections.immutable;
 
 public class HalfCopy {
-    ImmutableLinkedList halfCopy;
-    Node uncopied;
+    private ImmutableLinkedList halfCopy;
+    private Node uncopied;
+    private Node copied;
 
-    public HalfCopy(ImmutableLinkedList halfCopy, Node copyIndex) {
+    public void setCopied(Node copied) {
+        this.copied = copied;
+    }
+
+    public Node getUncopied() {
+        return uncopied;
+    }
+
+    public Node getCopied() {
+        return copied;
+    }
+
+    public void setLastCopied(Node lastCopied) {
+        this.lastCopied = lastCopied;
+    }
+
+    Node getLastCopied() {
+        return lastCopied;
+    }
+
+    private Node lastCopied;
+
+    HalfCopy(ImmutableLinkedList halfCopy, Node uncopied, Node copied) {
         this.halfCopy = halfCopy;
-        this.uncopied = copyIndex;
+        this.uncopied = uncopied;
+        this.copied = copied;
+    }
+
+    public void setHalfCopy(ImmutableLinkedList halfCopy) {
+        this.halfCopy = halfCopy;
+    }
+
+    public void setUnCopied(Node uncopied) {
+        this.uncopied = uncopied;
+    }
+
+    ImmutableLinkedList getHalfCopy() {
+        return halfCopy;
+    }
+
+    Node getUnCopied() {
+        return uncopied;
     }
 }
